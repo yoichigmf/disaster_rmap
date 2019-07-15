611fdf8345dbdbdeef4cc5ccc42a6de9e6f2ad7a
yoichi_gmf <yoichi.kayama@gmail.com>
2019-07-16 05:46:38 +0900

    debug

--- index.php
+++ index.php
@@ -203,7 +203,7 @@ $spreadsheetId = getenv('SPREADSHEET_ID');
 $sheetd = GetSheet( $spreadsheetId, $sheetname ); 
  
 echo "<script>\n";
-#echo "tgjson=\"{\"type\": \"FeatureCollection\",\"name\": \"調査地点\",\"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:OGC:1.3:CRS84\" } },\"features\": [\" \n";
+#echo "tgjson=\"{\"type\": \"FeatureCollection\",\"name\": \"調査地点\",\"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:OGC:1.3:CRS84\" } },\"features\": [\" ";
 
 echo sprintf('tgjson="{\\"type\\":\\"FeatureCollection\\",\\"name\\":\\"調査地点\\",\\"crs\\":{ \\"type\\": \\"name\\", \\"properties\\": { \\"name\\": \\"urn:ogc:def:crs:OGC:1.3:CRS84\\" } },\\"features\\": [  \";\n ');
 foreach ($sheetd as $index => $cols) {
@@ -222,9 +222,9 @@ foreach ($sheetd as $index => $cols) {
      }
      
      
-     $itemd = " \"type\":\"Feature\",\"properties\":{\"日付\":\"${dated}\",\"ユーザ\":\"${userd}\"},\"geometry\":{\"type\": \"Point\", \"coordinate\":[null,null]}\n";
+     $itemd = " \\"type\\":\"Feature\",\"properties\":{\"日付\":\"${dated}\",\"ユーザ\":\"${userd}\"},\"geometry\":{\"type\": \"Point\", \"coordinate\":[null,null]}\n";
 
-     echo "tgjson=tgjson+${topc}+${itemd}\n";
+     echo "\ntgjson=tgjson+${topc}+${itemd};\n";
      }
    // echo sprintf('#%d >> "%s"', $index+1, implode('", "', $cols)).PHP_EOL;
  }
